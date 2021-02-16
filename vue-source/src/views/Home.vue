331124a94f98e0660a63f8da8de65b2d06483dc8
wyatt_whorton <wyatt_whorton@dpsk12.org>
2021-02-16 13:16:22 -0700

    Vue Implimentation

---
+++ vue-source/src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home" id="app">
+    <div class="about-text">
+                <h1 style="padding-left: 4rem; padding-right: 4rem; padding-bottom: 1rem; font-weight: bolder;">About</h1>
+                <p style="padding-left: 4rem; padding-right: 4rem;">From an innocent conversation on twitter to a reality, we are making the 3rd Homebrew contest for the PSVita. These past few days/months have been incredible for the PSVita. From a silent scene to an outburst of unlocked possibilities
+                    like the dreamcast emulators, GTA games on PSVita and sonic games being ported, Piglet (GLES 2.0 library) found and VitaGL being more complete than ever, there has been a sudden change in the PSVita scene for the better. This is why
+                    we have decided to call the contest KyûHEN (which means sudden change in Japanese, as well as keeping the HEN tradition like HENkaku,TaiHEN, GekiHEN). We want to help the vita scene to pursue that change by growing the interest about
+                    the homebrew scene with this contest. Calling people to create new contents for the PSVita using the existing tools like VitaGL, SDL, Unity, Piglet for games, and the vita/dolce SDK for the utility. If you want to be part of the journey
+                    and contribute with your project, then welcome to the KyûHEN contest!</p>
+            </div>
+            <div class="container p-5" style="text-align: left;">
+                <div class="row">
+                    <div class="col-md-6 p-5">
+                        <h3 style="font-weight: bolder;">Samilop "Cimmerian" Iter</h3>
+                        <p>
+                            I am one of the moderators of Custom Protocol (French hacking websites) as well as a former GekiHEN creator. I love using my knowledge to assist people navigating the Vita hacking scene in the henkaku and vita nuova discord’s and through my Vita troubleshooting
+                            guides. I frequently contribute to homebrew projects with beta testing and translation. You can contact me at my Discord and Twitter
+
+                        </p>
+                    </div>
+                    <div class="col-md-6 p-5">
+                        <h3 style="font-weight: bolder;">2Old4Gaming</h3>
+                        <p>
+                            My name is Sandeep Rai, also known as “2 Old 4 Gaming” on twitter and YouTube. I have been a gamer for most of my life. I have fond memories of sitting in front the TV and playing my NES with my brother, but as an adult I’ve found handheld gaming just
+                            suited my life better. The PlayStation Vita, with its varied library of games and portability, has been my device of choice when it comes to gaming. My passion for the Vita and its games inspired me to not only write a series
+                            of books about the history of the system but also start my YouTube channel You can Find me at my twitter, and youtube.
+
+                        </p>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-6 p-5">
+                        <h3 style="font-weight: bolder;">frangarcj</h3>
+                        <p>
+                            One of the maintainers of VITASDK and main Retroarch PS Vita developer. I am also involved in the Vita3K emulator development. I have been active in the scene since Rejuvenate times but real life has reduced my time available for development. Nowadays
+                            my main contributions are helping and assisting other developers in the henkaku and vita nuova discords. You can find me at my twitter, and github.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="contact">
+                <h1 style="padding-left: 4rem; padding-right: 4rem; padding-bottom: 1rem; font-weight: bolder;">Contact</h1>
+                <p style="padding-left: 4rem; padding-right: 4rem;">WOW CONTACT STUFF GOES HERE</p>
+            </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
